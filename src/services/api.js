ead77efc9b90d96fadd02e5fa9e38fbb7e19fb86
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-07-29 07:59:34 -0300

    version config

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://wazex.herokuapp.com',
+  baseURL: 'https://wazex-backend.herokuapp.com/devs',
 });
 
 export default api;
